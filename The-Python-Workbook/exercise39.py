4f491ad2b6ae624f1b9e7f8ac7b0ecf1e6f751dd
Emiliano <Emiliano@iMac-Emiliano.local>
2021-06-17 08:39:20 +0200

    completed exercise 39

--- The-Python-Workbook/exercise39.py
+++ The-Python-Workbook/exercise39.py
@@ -1,3 +1,16 @@
 """ 
-Write a program that determines the name of a shape from its number of sides. Read the number of sides from the user and then report the appropriate name as part of a meaningful message. Your program should support shapes with anywhere from 3 up to (and including) 10 sides. If a number of sides outside of this range is entered then your program should display an appropriate error message.
+The length of a month varies from 28 to 31 days. In this exercise you will create a program that reads the name of a month from the user as a string. Then your program should display the number of days in that month. Display “28 or 29 days” for February so that leap years are addressed.
 """
+
+mounth = (input('Insert mounth: '))
+
+
+if(mounth == 'January' or mounth == 'March' or mounth == 'May'or mounth == 'July' or mounth == 'August' or mounth == 'January' or mounth == 'October' or mounth == 'December'):
+    days = '31'
+elif(mounth == 'February'):
+    days = '28 or 29'
+else:
+    days = '30'
+
+print('The mounth of ' + mounth + ' have ' + days +' days')
+
